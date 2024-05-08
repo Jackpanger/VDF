e4243453bccec9c53d84f1b5daa88ea4fabac408
Jack Pang <jp6862@nyu.edu>
2024-05-07 20:04:45 -0400

    class group

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 from RSAGroup import *
 from ClassGroup import *
 from VDF import *
-if __name__ == '__main__':
-    # Test for RSA group
-    bits = 2048
-    T = 3000
+import unittest
 
-    """group 1 """
-    # RSA group
-    # group = RSAGroup(T, bits)
 
-    """group 2"""
-    # class group
-    discriminant = -131653324254138636653163861414331698305531090221496467927360326686715180966094250598321899621249972220387687148397451395672779897144571112116763666653213748473909547482437246405018707472153290116227072825447643324530509016778432769802300913461285128339119844239772697652504835780459732685000796733645621728639
-    a, b = 2, 1
-    c = (b * b - discriminant) // (4 * a)
-    group = ClassGroup(a, b, c, discriminant, T, 1024)
-
-    VDF = VDF(group)
-
-    # prover
-    # t = 2^T, y = x^{2^T}
+def testVDF(VDF, group):
     y = VDF.vdf_eval()  # send y to verifier
     print("y: " + str(y))
 
@@ -33,6 +17,31 @@ if __name__ == '__main__':
     print("pi: " + str(pi))
 
     # verifier
-    print("check: " + str(VDF.vdf_verify(pi, y)))
+    return VDF.vdf_verify(pi, y)
 
+
+class TestVDF(unittest.TestCase):
+    def setUp(self):
+        self.T = 3000
+
+    def test_rsa_group(self):
+        """group 1 """
+        # RSA group
+        bits = 2048
+        group = RSAGroup(self.T, bits)
+        vdf = VDF(group)
+        self.assertTrue(testVDF(vdf, group))
+        print("Verified successfully")
+
+    def test_class_group(self):
+        """group 2"""
+        # class group
+
+        discriminant = -131653324254138636653163861414331698305531090221496467927360326686715180966094250598321899621249972220387687148397451395672779897144571112116763666653213748473909547482437246405018707472153290116227072825447643324530509016778432769802300913461285128339119844239772697652504835780459732685000796733645621728639
+        a, b = 2, 1
+        c = (b * b - discriminant) // (4 * a)
+        group = ClassGroup(a, b, c, discriminant, self.T, 1024)
+        vdf = VDF(group)
+        self.assertTrue(testVDF(vdf, group))
+        print("Verified successfully")
 
